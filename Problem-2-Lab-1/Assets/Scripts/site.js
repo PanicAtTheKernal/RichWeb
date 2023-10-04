8efabcffbb0f27089b2d46c2076744be1ceb4c88
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2023-10-04 09:35:40 +0100

    Add "add note" function

--- Problem-2-Lab-1/Assets/Scripts/site.js
+++ Problem-2-Lab-1/Assets/Scripts/site.js
@@ -13,6 +13,11 @@ class Notes {
         this.renderNotes();
     }
 
+    addNote(content, colour) {
+        this.notes.push({text: content, colour: colour});
+        this.renderNotes()
+    }
+
     renderNotes() {
         const noteArea = document.getElementById("noteArea");
 
@@ -43,3 +48,45 @@ class Notes {
 }
 
 const notes = new Notes([{text: "test Note", colour: "red"}, {text: "Another note", colour: "blue"}]);
+createColourSelection();
+
+function submitNote() {
+    const noteContent = document.forms["noteForm"]["noteContent"].value;
+    const noteColour = document.forms["noteForm"]["noteColour"].value;
+    const noteDialog = document.getElementById("noteFormDialog");
+    notes.addNote(noteContent, noteColour);
+    noteDialog.close();
+}
+
+function openNoteDialog() {
+    const noteDialog = document.getElementById("noteFormDialog");
+    noteDialog.show();
+}
+
+function createColourSelection() {
+    const colourSelection = document.getElementById("colourSelection");
+    const colourKeys = Object.keys(colours);
+    const title = document.createElement("label")
+    title.textContent = "Select colour:";
+
+    colourSelection.appendChild(title);
+    colourSelection.appendChild(document.createElement("br"));
+    
+    colourKeys.forEach((key, index) => {
+        const radioButton = document.createElement("input");
+        const label = document.createElement("label");
+
+        radioButton.type = "radio";
+        radioButton.id = key;
+        radioButton.name = "noteColour";
+        radioButton.value = key;
+        if (index === 0) radioButton.checked = true;
+
+        label.setAttribute("for", key);
+        label.textContent = key;
+
+        colourSelection.appendChild(radioButton);
+        colourSelection.appendChild(label);
+        colourSelection.appendChild(document.createElement("br"));
+    });
+}
