18ce822fa574bd748f6e394a466bd9aa54f3dcbc
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2023-10-04 10:50:56 +0100

    Create extension to show all links on a page

--- Problem-3-Lab-1/Content.js
+++ Problem-3-Lab-1/Content.js
@@ -1,39 +1,51 @@
-function getAllVideos() {
-    const allVideos = document.getElementsByTagName('video');
-    const divs = document.createElement('div');
-    const list = document.createElement('ul');
+function getAllLinks() {
+    const allLinks = document.getElementsByTagName("a");
+    const div = document.createElement("div");
+    const list = document.createElement("ul");
 
-    // for (let i = 0; i < allVideos.length; i++) {
-    //     const listItem = document.createElement('li');
-    //     const button = document.createElement('button');
+    if (allLinks.length == 0) {
+        div.innerText = "No links found";
+        return div;
+    }
 
+    for (let i = 0; i < allLinks.length; i++) {
+        const listItem = document.createElement("li");
+        const link = document.createElement("a");
+        link.title = allLinks[i].title;
+        link.innerText = allLinks[i].innerText;
+        link.href = allLinks[i].href;
 
-    //     listItem.
-    // }
+        listItem.appendChild(link);
+        list.appendChild(listItem);
+    }
+
+    div.appendChild(list);
+    return div;
 }
 
-function showVideo(videoLink) {
+function createlinks() {
+    const links = document.createElement("div");
 
-}
+    links.id = "links";
+    links.style.position = "fixed";
+    links.style.right = "10px";
+    links.style.bottom = "10px";
+    links.style.margin = "0px";
+    links.style.padding = "10px";
+    links.style.width = "fitContent";
+    links.style.maxHeight = "200px";
+    links.style.overflow = "scroll";
+    links.style.backgroundColor = "White";
+    links.style.border = "1px solid black";
 
-function createVideoPlayer() {
-    const videoPlayer = document.createElement('div');
+    links.appendChild(getAllLinks());
 
-    videoPlayer.id = "videoPlayer";
-    videoPlayer.style.position = "fixed";
-    videoPlayer.style.right = "10px";
-    videoPlayer.style.top = "10px";
-    videoPlayer.style.margin = "0px";
-    videoPlayer.style.backgroundColor = "White";
-    videoPlayer.style.width = "100px";
-    videoPlayer.style.height = "100px";
-
-    return videoPlayer;
+    return links;
 }
 
 function createShowButton() {
-    let dialog = document.createElement('dialog');
-    let showButton = document.createElement('button');
+    let dialog = document.createElement("dialog");
+    let showButton = document.createElement("button");
 
     showButton.setAttribute("show", "true");
     showButton.id = "ShowButton";
@@ -41,10 +53,10 @@ function createShowButton() {
     showButton.onclick = () => {
         const buttonRef = document.getElementById("ShowButton");
         if(buttonRef.getAttribute("show") == "true") {
-            document.getElementById("videoPlayer").style.display = "none";
+            document.getElementById("links").style.display = "none";
             showButton.setAttribute("show", "false");
         } else {
-            document.getElementById("videoPlayer").style.display = "block";
+            document.getElementById("links").style.display = "block";
             showButton.setAttribute("show", "true");
         }
     }
@@ -61,8 +73,8 @@ function createShowButton() {
 }
 
 function extension() {
-    const currentBody = document.getElementsByTagName('body')[0];
-    currentBody.appendChild(createVideoPlayer())
+    const currentBody = document.getElementsByTagName("body")[0];
+    currentBody.appendChild(createlinks())
     currentBody.appendChild(createShowButton());
 }
 
