1b8e32fcee3bd60d8b467b01a7b0117af8af9a06
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2023-11-09 23:35:27 +0000

    Finish the timer

--- Lab-3/Problem-2/Assets/Scripts/site.js
+++ Lab-3/Problem-2/Assets/Scripts/site.js
@@ -1,6 +1,53 @@
-let state = false;
+function startCountdown() {
+    const hours = parseInt(document.forms["countdownForm"]["hours"].value);
+    const minutes = parseInt(document.forms["countdownForm"]["minutes"].value);
+    const seconds = parseInt(document.forms["countdownForm"]["seconds"].value);
+    let totalSeconds = 0;
 
-function buttonClick() {
-    document.getElementById("text").style.color = (state) ? "blue" : "black";
-    state = !state;
+    if (seconds > 0) {
+        totalSeconds = seconds;
+    }
+
+    if (minutes > 0) {
+        totalSeconds += minutes * 60;
+    }
+
+    if (hours > 0) {
+        totalSeconds += hours * 3600;
+    }
+
+    const timeInterval = rxjs.interval(1000);
+    const timeObservable = timeInterval.pipe(rxjs.take(totalSeconds+1));
+
+    timeObservable.subscribe({
+        next(value) {
+        console.log(value);
+        displayTime(totalSeconds);
+        totalSeconds -= 1;
+    }})
 }
+
+function displayTime(totalSeconds) {
+    const timer = document.getElementById("countdown");
+    const hoursRem = Math.floor(totalSeconds / 3600);
+    totalSeconds -= hoursRem * 3600;
+    const minutesRem = Math.floor((totalSeconds / 60));
+    totalSeconds -= minutesRem * 60;
+    const secondsRem = totalSeconds;
+    const secondsStr = (secondsRem < 10) ? `0${secondsRem}` : secondsRem;
+    let timeStr = `${secondsStr}`;
+    
+    if (minutesRem != 0) {
+        const minutesStr = (minutesRem < 10) ? `0${minutesRem}` : minutesRem;
+        timeStr = minutesStr + ":" + timeStr;
+    }
+
+    if (hoursRem != 0) {
+        const hoursStr = (hoursRem < 10) ? `0${hoursRem}`: hoursRem;
+        timeStr = hoursStr + ":" + timeStr;
+    }
+    timer.innerText = timeStr;
+}
+
+const startCountdownBtn = document.getElementById("startCountdown");
+rxjs.fromEvent(startCountdownBtn, "click").subscribe(() => startCountdown());
